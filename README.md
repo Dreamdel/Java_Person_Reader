a55ad2a7a683d85165b3596c37be0cccfbd64f34
Dreamdel <128373065+Dreamdel@users.noreply.github.com>
2023-12-02 15:42:11 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Java_Person_Reader
+# Java-Person-Reader
+[![Javadoc](https://img.shields.io/badge/JavaDoc-Online-green)](https://L1SP.github.io/Java-Person-Reader/javadoc/)
---
+++ src/main/java/com/kosm/Enums/Gender.java
@@ -0,0 +1,15 @@
+package com.kosm.Enums;
+
+/**
+ * Gender enum containing two genders(Male and Female) [Non-binaries, forgive me, please]
+ */
+public enum Gender { 
+	/**
+	 * Male gender
+	 */
+	Male,
+	/**
+	 * Female gender
+	 */
+	Female
+}
